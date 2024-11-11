bc87c5b557be989aae268cdb18939862ca03c49e
RhodCoding <joewee321@gmail.com>
2024-11-11 11:55:15 +0800

    Fix Bug (1)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,8 +10,8 @@
 		</h1>
 		<ul class="flex justify-around w-[30%] items-center text-[24px]">
 			<li><a href="/">Home</a></li>
-			<li><a href="/shop">Features</a></li>
-			<li><a href="/visit-us">Visit Us</a></li>
+			<li><a href="/">Features</a></li>
+			<li><a href="/">Visit Us</a></li>
 		</ul>
 		<button class="text-[24px]">
 			Login
