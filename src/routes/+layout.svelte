2a00d795bd3b23f2cd7451087048f949cae44a86
RhodCoding <joewee321@gmail.com>
2024-11-11 11:51:45 +0800

    Add Content and Use Tailwind css

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+<main>
+	<nav class="sticky top-0 bg-[#FFF8E8] flex justify-around items-center h-[70px] drop-shadow">
+		<h1 class="text-[30px] font-bold">
+			Enteng Bakery
+		</h1>
+		<ul class="flex justify-around w-[30%] items-center text-[24px]">
+			<li><a href="/">Home</a></li>
+			<li><a href="/shop">Features</a></li>
+			<li><a href="/visit-us">Visit Us</a></li>
+		</ul>
+		<button class="text-[24px]">
+			Login
+		</button>
+	</nav>
+{@render children()}
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<div class="h-[499px] bg-[#FFF8E8] flex justify-around">
+    <div class="w-[594px] h-[420px] mt-5 flex flex-col items-start justify-around">
+      <h1 class="text-[40px] text-[#FF8343] font-bold">
+         ENTENG BAKERY
+      </h1>
+      <p class="text-[30px]">
+         Baking is a science, but it’s also a form of art. It’s about creating something delicious and beautiful that makes people happy
+      </p>
+      <button class="text-[30px] p-[10px] bg-[#FF8343] rounded text-white">
+          LEARN MORE
+      </button>
+    </div>
+    <img src="/assets/bread.png" alt="" class="w-[594px] h-[420px] mt-5">
+</div>
